637054ee80714a90e824eb10f524ff0a7377c7af
Baezor <64096464+Baezor1@users.noreply.github.com>
2024-03-27 09:27:20 -0500

    ✨ Move Arm Command

---
+++ src/main/java/frc/robot/commands/arm/MoveArmCmd.java
@@ -0,0 +1,52 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.arm;
+
+import frc.robot.Constants;
+import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.utils.Direction;
+import edu.wpi.first.wpilibj2.command.Command;
+
+/** An example command that uses an example subsystem. */
+public class MoveArmCmd extends Command {
+	@SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+	private final ArmSubsystem arm;
+	private final Direction direction;
+
+	/**
+	 * Creates a new Arm Raise Command.
+	 *
+	 * @param subsystem The arm subsystem used by this command.
+	 * @param direction The direction the arm will move.
+	 */
+	public MoveArmCmd(ArmSubsystem subsystem, Direction direction) {
+		arm = subsystem;
+		this.direction = direction;
+		// Use addRequirements() here to declare subsystem dependencies.
+		addRequirements(subsystem);
+	}
+
+	// Called when the command is initially scheduled.
+	@Override
+	public void initialize() {}
+
+	// Called every time the scheduler runs while the command is scheduled.
+	@Override
+	public void execute() {
+		arm.moveArm(direction);
+	}
+
+	// Called once the command ends or is interrupted.
+	@Override
+	public void end(boolean interrupted) {
+		arm.stopArm();
+	}
+
+	// Returns true when the command should end.
+	@Override
+	public boolean isFinished() {
+		return false;
+	}
+}

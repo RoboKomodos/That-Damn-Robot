d53f020acbfe5a60e6cfcd7dec924425d5555fd5
Baezor <64096464+Baezor1@users.noreply.github.com>
2024-03-27 09:27:29 -0500

    ✨ Move Climber Command

---
+++ src/main/java/frc/robot/commands/climber/MoveClimberCmd.java
@@ -0,0 +1,46 @@
+package frc.robot.commands.climber;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ClimberSubsystem;
+import frc.robot.utils.Direction;
+
+public class MoveClimberCmd extends Command {
+	@SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+	private final ClimberSubsystem climber;
+
+    private final Direction direction;
+
+	/**
+	 * Creates a new Arm Raise Command.
+	 *
+	 * @param subsystem The arm subsystem used by this command.
+	 * @param direction The direction the arm will move.
+	 */
+	public MoveClimberCmd(ClimberSubsystem subsystem, Direction direction) {
+		climber = subsystem;
+        this.direction = direction;
+		// Use addRequirements() here to declare subsystem dependencies.
+		addRequirements(subsystem);
+	}
+
+	// Called when the command is initially scheduled.
+	@Override
+	public void initialize() {
+		// Call toggle once
+		climber.moveClimber(direction);
+	}
+
+	// Called every time the scheduler runs while the command is scheduled.
+	@Override
+	public void execute() {}
+
+	// Called once the command ends or is interrupted.
+	@Override
+	public void end(boolean interrupted) {}
+
+	// Returns true when the command should end.
+	@Override
+	public boolean isFinished() {
+		return false;
+	}
+}
